745e6e989a7994bec3e7905cb740727c6520cea9
Temidayo99 <oyelamitemidayo99@gmail.com>
2020-11-04 09:42:34 +0100

    modified files

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//function myFunction() {
-//  var x = document.getElementById("myLinks");
-//  if (x.style.display === "block") {
-//    x.style.display = "none";
-//  } else {
-//    x.style.display = "block";
-//  }
-//}
-
 function openNav() {
   document.getElementById("myLinks").style.width = "100%";
 }
