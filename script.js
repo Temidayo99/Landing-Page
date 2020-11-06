0c99076e66aec42b2aacc2f98b39d53b6643129b
Temidayo99 <oyelamitemidayo99@gmail.com>
2020-11-06 21:26:06 +0100

    modified files

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//function to open the hamburger menu
 function openNav() {
   document.getElementById("myLinks").style.width = "100%";
 }
 
+//function to close the hamburger menu
 function closeNav() {
   document.getElementById("myLinks").style.width = "0";
 }
@@ -12,20 +14,24 @@ var bodyBg = document.getElementById("main_container");
 
 function lightblue(){
   bodyBg.setAttribute("id", "bluediv");
-  
+  document.getElementById("content").style.color = "#fff";
+  document.getElementById("button").style.background = "#ED48BA";
+  document.getElementById("button").style.color = "#fff";
 }
 function pink(){
   bodyBg.setAttribute("id", "pinkdiv");
-  document.getElementById("nav_container").style.color = "black";
-  document.getElementById("button").style.background = "white";
-  document.getElementById("button").style.color = "black";
+  document.getElementById("button").style.background = "#fff";
+  document.getElementById("button").style.color = "#ED48BA";
 }
 function yellow(){
   bodyBg.setAttribute("id", "yellowdiv");
   document.getElementById("content").style.color = "black";
+  document.getElementById("button").style.background = "white";
+  document.getElementById("button").style.color = "black";
 }
 function white(){
   bodyBg.setAttribute("id", "whitediv");
   document.getElementById("content").style.color = "black";
-  document.getElementById("nav_container").style.color = "black";
+  document.getElementById("button").style.background = "#ED48BA";
+  document.getElementById("button").style.color = "#fff";
 }
